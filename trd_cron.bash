96a5fb34a33bc8140b8f7fa14a999be07ae46e86
Geoff Cleary <gcleary@llnl.gov>
2017-07-07 14:47:15 -0700

    Updating copyright statements in source files.

--- trd_cron.bash
+++ trd_cron.bash
@@ -1,18 +1,25 @@
 #!/usr/bin/env bash
 
-#   Copyright 2017 Lawrence Livermore National Laboratory
+# Copyright (c) 2017, Lawrence Livermore National Security, LLC.
+# Produced at the Lawrence Livermore National Laboratory
+# Written by Geoff Cleary <gcleary@llnl.gov>.
+# LLNL-CODE-734257
 #
-#   Licensed under the Apache License, Version 2.0 (the "License");
-#   you may not use this file except in compliance with the License.
-#   You may obtain a copy of the License at
-#
-#       http://www.apache.org/licenses/LICENSE-2.0
-#
-#   Unless required by applicable law or agreed to in writing, software
-#   distributed under the License is distributed on an "AS IS" BASIS,
-#   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-#   See the License for the specific language governing permissions and
-#   limitations under the License.
+# All rights reserved.
+# This file is part of Tour Robot Dance. For details, see
+# https://github.com/LLNL/STKRobotDance. Licensed under the
+# Apache License, Version 2.0 (the “Licensee”); you may not use
+# this file except in compliance with the License. You may
+# obtain a copy of the License at:
+# 
+# http://www.apache.org/licenses/LICENSE-2.0
+# 
+# Unless required by applicable law or agreed to in writing,
+# software distributed under the License is distributed on an
+# “AS IS” BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND,
+# either express or implied. See the License for the specific
+# language governing permissions and limitations under the license.
+
 
 exec_dir="/usr/bin"
 lock_file="/tmp/trd_cron.running"
